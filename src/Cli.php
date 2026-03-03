d28dd88b10506b66d40c50498dce6e1f7a9b01d1
dromomania <ledavet.ta@gmail.com>
2026-03-03 11:51:39 +0300

    Add CLI greetng feature

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\prompt;
+# use function cli\;
+function greetingUser() {
+	$name = prompt("May I have your name?");
+	$greeting = "Hello, $name!";
+	echo $greeting;
+}
+
+?>
