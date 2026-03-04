9e91fbe46554e394211521e55e88c75adec9d101
dromomania <ledavet.ta@gmail.com>
2026-03-04 23:36:10 +0300

    Integrate and configure PHP_CodeSniffer

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ brain-games:
 	./bin/brain-games
 validate:
 	composer validate
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
+lint-fix:
+	composer exec --verbose phpcbf -- --standard=PSR12 --colors src bin
--- src/Cli.php
+++ src/Cli.php
@@ -3,11 +3,11 @@
 namespace BrainGames\Cli;
 
 use function cli\prompt;
-# use function cli\;
-function greetingUser() {
-	$name = prompt("May I have your name?");
-	$greeting = "Hello, $name!";
-	echo $greeting;
-}
 
-?>
+# use function cli\;
+function greetingUser()
+{
+    $name = prompt("May I have your name?");
+    $greeting = "Hello, $name!";
+    echo $greeting;
+}
