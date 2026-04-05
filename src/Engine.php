0b0285e80361e871a1a8e1abd52ae428f6ab5384
dromomania <ledavet.ta@gmail.com>
2026-04-05 21:09:21 +0300

    Fix linting errors

--- src/Engine.php
+++ src/Engine.php
@@ -1,25 +1,25 @@
 <?php
+
 namespace BrainGames\Engine;
 
 use function cli\out;
 
-function isCorrectAnswer($expected, $answer) {
-        if ($expected === $answer) {
-                echo out("Correct! \n");
-                return true;
-        } else {
-                echo out("'$answer' is wrong answer ;(. Correct answer was '$expected'. \n");
-                return false;
-        }
+function isCorrectAnswer($expected, $answer)
+{
+    if ($expected === $answer) {
+            echo out("Correct! \n");
+            return true;
+    } else {
+            echo out("'$answer' is wrong answer ;(. Correct answer was '$expected'. \n");
+            return false;
+    }
 }
 
-function endGame($counterCorrectAnswers, $name) {
-        if ($counterCorrectAnswers === 3) {
-                echo out("Congratulations, $name!\n");
-        } else {
-                echo out("Let's try again, $name!\n");
-        }
+function endGame($counterCorrectAnswers, $name)
+{
+    if ($counterCorrectAnswers === 3) {
+            echo out("Congratulations, $name!\n");
+    } else {
+            echo out("Let's try again, $name!\n");
+    }
 }
-
-
-?>
